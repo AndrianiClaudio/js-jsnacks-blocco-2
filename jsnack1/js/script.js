f2c8bd9e40198338dc49ae0dca6a5f711f69badc
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-30 15:25:00 +0100

    js-1: campionamento casuale del nome

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,7 +1,10 @@
 //Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 //PRENDO UNA LISTA DI NOMI
-//PRENDO UNA LISTA DI COGNOMI
 const firstName = ['Claudio', 'Giuseppe', 'Francesca', 'Chiara', 'Elisabetta', 'Thomas', 'Gianfranco', 'Bogdan'];
+//PRENDO UNA LISTA DI COGNOMI
 const surname = ['Andriani','Pater','Gatti','Passaro','Daho','De Luca','Zola','Marian'];
 //si presume le lunghezze degli array saranno uguali
 const length = firstName.length;
@@ -10,4 +13,7 @@ const fullname = []
 for(let i=0;i<length;i++) {
     fullname[i]=firstName[i] + ' ' + surname[i];
     console.log(`Invitato #${i + 1}: ${fullname[i]}`);
+}
+for (let i = 0; i < fullname.length; i++) {
+    console.log(fullname[getRndInteger(0,firstName.length -1)]);
 }
